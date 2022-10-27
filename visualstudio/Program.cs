11b61931c49eb6c888467bf184e3b2086cb55826
Isma-e <a2208437@institutMontilivi.cat>
2022-10-27 19:41:20 +0200

    ismael

---
+++ visualstudio/Program.cs
@@ -0,0 +1,18 @@
+﻿//declaració  de variables
+int a, b, x;
+
+//valors d'entrada
+Console.WriteLine("Escriu el valor de a");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Escriu el valor de b");
+b = Convert.ToInt32(Console.ReadLine());
+// algoritme
+if (a == 0)
+    Console.WriteLine("Aquesta operacio no es pot fer, Escull un altre numero que no sigui 0");
+else
+{
+    x = -b / a;
+    // valors de sortida
+    Console.WriteLine($"x ={x}");
+}
+Console.ReadKey();
